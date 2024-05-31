b8df8931eda6231dcf1a415f3f4d0e6e88ba4fb2
Vaibhav Umbarkar <vaibhavumbarkar8485@gmail.com>
2024-06-01 01:02:02 +0530

    v1.3

--- lib/order.py
+++ lib/order.py
@@ -1,10 +1,96 @@
+# import os
+# from lib.api import API
+# from lib.alert import send_order_alert, send_alert
+
+# # Authentication keys
+# secret_key = os.environ.get('SECRETKEY')
+# api_key = os.environ.get('APIKEY')
+
+# # Create API Object
+# api_connector = API(secret_key, api_key)
+
+# user_portfolio = api_connector.get_user_portfolio()
+
+# def get_main_balance():
+#     for data in user_portfolio['data']:
+#         if data['currency'] == 'INR':
+#             return float(data['main_balance'])
+#     return 0.0
+
+# def find_coin(coin_name):
+#     for data in user_portfolio['data']:
+#         if data['currency'] == coin_name:
+#             return True
+#     return False
+
+# def get_coin_quantity(coin_name):
+#     for data in user_portfolio['data']:
+#         if data['currency'] == coin_name:
+#             return float(data['main_balance'])
+#     return 0.0
+
+
+# def place_order(side, symbol, price):
+#     coin_name = symbol.split('/')[0]  # Convert coin name e.g. BTC/INR => BTC
+#     balance = get_main_balance()  # Get Fund or main balance from portfolio
+#     coin_exists = find_coin(coin_name)  # Check the coin exists in portfolio or not
+    
+#     if side == 'buy':
+#         buy_quantity = 100 / price  # Calculate buy quantity (Note: Always invest 100 INR from portfolio balance)
+#         if balance >= 110:
+#             payload = {
+#                 "side": side,
+#                 "symbol": symbol,
+#                 "type": "limit",
+#                 "price": price,
+#                 "quantity": buy_quantity,
+#                 "exchange": "coinswitchx"
+#             }
+#             response = api_connector.create_order(payload=payload)
+#             if response.get("status") == "success":
+#                 send_order_alert(side, symbol, price, buy_quantity, balance)
+#             else:
+#                 send_alert(f"Buy Order failed: {response.get('message', 'Unknown error')}")
+#         else:
+#             send_alert("Insufficient funds for trade! Please add more than 110 INR.")
+    
+#     elif side == 'sell' and coin_exists:
+#         sell_quantity = get_coin_quantity(coin_name)  # Get coin quantity from portfolio
+#         if sell_quantity > 0:
+#             payload = {
+#                 "side": side,
+#                 "symbol": symbol,
+#                 "type": "limit",
+#                 "price": price,
+#                 "quantity": sell_quantity,
+#                 "exchange": "coinswitchx"
+#             }
+#             response = api_connector.create_order(payload=payload)
+#             if response.get("status") == "success":
+#                 send_order_alert(side, symbol, price, sell_quantity, balance)
+#             else:
+#                 send_alert(f"Sell Order failed: {response.get('message', 'Unknown error')}")
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 from lib.api import API
 from lib.alert import send_order_alert, send_alert
 
 # Authentication keys
-secret_key = os.environ.get('SECRETKEY')
-api_key = os.environ.get('APIKEY')
+# secret_key = os.environ.get('SECRETKEY')
+secret_key = "8517b4a7e07104f5605d03680859fb88ea03486608beadc093155f258904019c"
+# api_key = os.environ.get('APIKEY')
+api_key = "df7e0c08e87cf7d974a8acb2044ba36412b6c909a7d3a6cb073097a03a726f48"
 
 # Create API Object
 api_connector = API(secret_key, api_key)
--- script.py
+++ script.py
@@ -17,6 +17,7 @@ def CRYPTO_BOT():
             # Get Conclusion
             result_1d, b1_1d, s1_1d, h1_1d, b2_1d, s2_1d, h2_1d = get_signal(df_1d)
             result_1w, b1_1w, s1_1w, h1_1w, b2_1w, s2_1w, h2_1w = get_signal(df_1w)
+            
             # Cal Final Result
             if result_1d == 1 and result_1w == 1:
                 place_order('buy', asset, price)
@@ -24,6 +25,7 @@ def CRYPTO_BOT():
             elif result_1d == 0 and result_1w == 0:
                 place_order('sell', asset, price)
                 send_action_alert('Sell', asset, price, result_1d, result_1w, s1_1d, s2_1d, s1_1w, s2_1w, h1_1d, h2_1d, h1_1w, h2_1w)
+                
         print("[End]")
     except Exception as e:
         print(f"An unexpected error occurred in Crypto Bot : {e}")
