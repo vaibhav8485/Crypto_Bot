6bf76db18595c0714d1875fc9e720e94c5f1f141
Vaibhav Umbarkar <vaibhavumbarkar8485@gmail.com>
2024-06-16 14:14:26 +0530

    v1.8

--- lib/dataset.py
+++ lib/dataset.py
@@ -41,3 +41,26 @@ def get_data(symbol, timeframe, limit, retries=5, delay=5):
     # Return an empty DataFrame in case of failure
     logging.error(f"Failed to fetch data for {symbol} after {retries} attempts.")
     return pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+
+# Convert Weekly to Monthly DF
+def get_month_data(weekly_data):
+    try:
+        # Check if 'timestamp' is in the index
+        if not isinstance(weekly_data.index, pd.DatetimeIndex):
+            raise ValueError("'timestamp' index not found or not in datetime format.")
+
+        # Convert the weekly data into monthly data using resample on the index
+        df = weekly_data.resample('M').agg({
+            'open': 'first',
+            'high': 'max',
+            'low': 'min',
+            'close': 'last',
+            'volume': 'sum'
+        })
+        return df
+
+    except ValueError as ve:
+        print("ValueError:", ve)
+    except Exception as e:
+        print("Error occurred in One Month Data Conversion:", e)
+        
--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 import logging
 
 # User Define Modules
-from lib.dataset import get_data  # Get dataset
+from lib.dataset import get_data, get_month_data  # Get dataset
 from lib.signal import get_signal  # Get signal 
 from lib.order import place_order  # Place orders to coin switch exchange 
 
@@ -15,31 +15,33 @@ watchlist = ["BTC/INR", "ETH/INR", "MATIC/INR", "XRP/INR", "ADA/INR", "BAT/INR",
 # Main Function
 def CRYPTO_BOT():
     try:
-        logging.info("[Start]")
+        logging.info("[Start:Script]")
         for asset in watchlist:     
             
             df_1d = get_data(asset, '1d', 1000) # Get 1 Day OHLCV data
             df_1w = get_data(asset, '1w', 1000) # Get 1 Week OHLCV data
+            df_1m = get_month_data(df_1w) # Get 1 Month OHLCV data
             
             # Check if the dataframes are empty
             if df_1d.empty or df_1w.empty:
                 logging.warning(f"No data available for {asset}. Skipping.")
                 continue
 
-            result_1d,_,_,_,_,_,_,_,_ = get_signal(df_1d) # Get 1 Day Signal
-            result_1w, momentum_buy, momentum_sell, trend_buy, trend_sell, volatility_buy, volatility_sell, volume_buy, volume_sell = get_signal(df_1w) # Get 1 Week Signal
+            result_1m,_,_,_,_,_,_,_,_  = get_signal(df_1m) # Get 1 Month Signal
+            result_1w,_,_,_,_,_,_,_,_ = get_signal(df_1w) # Get 1 Week Signal
+            result_1d, momentum_buy, momentum_sell, trend_buy, trend_sell, volatility_buy, volatility_sell, volume_buy, volume_sell = get_signal(df_1d) # Get 1 Day Signal
             
-            if result_1d == 1 and result_1w == 1: # For BUY
+            if result_1d == 1 and result_1w == 1 and result_1m == 1: # For BUY
                 place_order('buy', asset, result_1d, result_1w, momentum_buy, trend_buy, volatility_buy, volume_buy, momentum_sell, trend_sell, volatility_sell, volume_sell)
                 logging.info(f"Buy order placed for {asset}")
-            elif result_1d == 0 and result_1w == 0: # For SELL
+            elif result_1d == 0 and result_1w == 0 and result_1m == 0: # For SELL
                 place_order('sell', asset, result_1d, result_1w, momentum_buy, trend_buy, volatility_buy, volume_buy, momentum_sell, trend_sell, volatility_sell, volume_sell)
                 logging.info(f"Sell order placed for {asset}")
 
-        logging.info("[End]")
+        logging.info("[End:Script]")
     except Exception as e:
         logging.error(f"An unexpected error occurred in Crypto Bot: {e}")
 
 # Execution Start form this
 if __name__ == "__main__":
-    CRYPTO_BOT()
+    CRYPTO_BOT() # Calling Main Function
