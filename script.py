f8ed7e8e8b0473955f49ba9c61aed9ee84f59e9b
Vaibhav Umbarkar <63337898+vaibhav8485@users.noreply.github.com>
2024-06-24 22:38:30 +0530

    Testing 1.2

--- script.py
+++ script.py
@@ -19,40 +19,40 @@ log_data = load_log_file(file_path) # Load Log File
 # Crypto Watch list (12 Assets)
 watchlist = ["BTC/INR", "ETH/INR", "MATIC/INR", "XRP/INR", "ADA/INR", "BAT/INR", "BNB/INR", "SOL/INR", "NEAR/INR", "SAND/INR", "DOGE/INR", "STX/INR"]
 
-# # Main Crypto Bot Function
-# def CRYPTO_BOT():
-#     try:
-#         logging.info("[Start: CRYPTO_BOT()]")
-#         for symbol in watchlist:
-#             crypto_exists = find_coin(symbol)  # Check the coin exists in portfolio or not
-#             current_price = ticker(symbol) # Get Current Price of Crypto
-#             # Auto Set Log Status
-#             auto_status_change(file_path, log_data, crypto_exists, symbol, current_price)
-#             # Get Log Status
-#             log_status = get_status(log_data, symbol)
+# Main Crypto Bot Function
+def CRYPTO_BOT():
+    try:
+        logging.info("[Start: CRYPTO_BOT()]")
+        for symbol in watchlist:
+            crypto_exists = find_coin(symbol)  # Check the coin exists in portfolio or not
+            current_price = ticker(symbol) # Get Current Price of Crypto
+            # Auto Set Log Status
+            auto_status_change(file_path, log_data, crypto_exists, symbol, current_price)
+            # Get Log Status
+            log_status = get_status(log_data, symbol)
                 
-#             df_1d = get_data(symbol, '1d', 1000) # Get 1 Day OHLCV data
-#             df_1w = get_data(symbol, '1w', 1000) # Get 1 Week OHLCV data
-#             df_1m = get_month_data(df_1w) # Get 1 Month OHLCV data
+            df_1d = get_data(symbol, '1d', 1000) # Get 1 Day OHLCV data
+            df_1w = get_data(symbol, '1w', 1000) # Get 1 Week OHLCV data
+            df_1m = get_month_data(df_1w) # Get 1 Month OHLCV data
             
-#             # Check if the dataframes are empty
-#             if df_1d.empty or df_1w.empty:
-#                 logging.warning(f"No data available for {symbol}. Skipping.")
-#                 continue
+            # Check if the dataframes are empty
+            if df_1d.empty or df_1w.empty:
+                logging.warning(f"No data available for {symbol}. Skipping.")
+                continue
 
-#             result_1d = get_signal(df_1d) # Get 1 Day Signal
-#             result_1w = get_signal(df_1w) # Get 1 Week Signal
-#             result_1m = get_signal(df_1m) # Get 1 Month Signal
+            result_1d = get_signal(df_1d) # Get 1 Day Signal
+            result_1w = get_signal(df_1w) # Get 1 Week Signal
+            result_1m = get_signal(df_1m) # Get 1 Month Signal
 
-#             if result_1d == 1 and result_1w == 1 and result_1m != 0 and log_status == 'auto'and crypto_exists == False: # For BUY
-#                 place_order('buy', symbol) # Place Buy Order
-#                 logging.info(f" Buy order placed for {symbol}") # Console Msg
-#             elif result_1d == 0 and result_1w == 0 and result_1m != 1 and log_status == 'auto' and crypto_exists == True: # For SELL
-#                 place_order('sell', symbol) # Place Sell Order
-#                 logging.info(f" Sell order placed for {symbol}") # Console Msg
-#         logging.info("[End: CRYPTO_BOT()]")
-#     except Exception as e:
-#         logging.error(f"An unexpected error occurred in Crypto Bot: {e}")
+            if result_1d == 1 and result_1w == 1 and result_1m != 0 and log_status == 'auto'and crypto_exists == False: # For BUY
+                place_order('buy', symbol) # Place Buy Order
+                logging.info(f" Buy order placed for {symbol}") # Console Msg
+            elif result_1d == 0 and result_1w == 0 and result_1m != 1 and log_status == 'auto' and crypto_exists == True: # For SELL
+                place_order('sell', symbol) # Place Sell Order
+                logging.info(f" Sell order placed for {symbol}") # Console Msg
+        logging.info("[End: CRYPTO_BOT()]")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred in Crypto Bot: {e}")
 
 # Main Risk Management Function
 def RISK_MANAGEMENT():
@@ -85,4 +85,4 @@ def RISK_MANAGEMENT():
 # Execution Start form this
 if __name__ == "__main__":
     RISK_MANAGEMENT() # Calling Risk Management Function 
-    # CRYPTO_BOT() # Calling Main Function
+    CRYPTO_BOT() # Calling Main Function
